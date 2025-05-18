5e1853a7eded9066a2d20686c5fa3229df3531b3
robert <Neorganizm911@yandex.ru>
2025-05-18 23:12:29 +0300

    3 step

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -15,15 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-from django.views.generic import TemplateView
 from django.urls import path, include
-from django.contrib import admin
 from django.contrib.auth import views as auth_views
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name='index.html'), name='home'),
-    path('', include('users.urls')),
-    path('admin/', admin.site.urls),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('users/', include('users.urls')),
 ]
