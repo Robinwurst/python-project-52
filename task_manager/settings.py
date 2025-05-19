13a0680a2fe5aade449b1117d76fb8d1ddd567d8
robert <Neorganizm911@yandex.ru>
2025-05-20 02:06:46 +0300

    4 step

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -37,7 +37,7 @@ ALLOWED_HOSTS = ['webserver', '127.0.0.1', 'localhost']
 # Application definition
 
 INSTALLED_APPS = [
-    # 'django.contrib.admin',
+    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
