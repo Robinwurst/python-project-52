21ee79eb21654fb31fea666069b171f9cdebbea3
robert <Neorganizm911@yandex.ru>
2025-06-23 20:05:30 +0300

    8 step

--- build.sh
+++ build.sh
@@ -1,27 +1,10 @@
 #!/usr/bin/env bash
-# Exit on error
-set -o errexit
 
-# Modify this line as needed for your package manager (pip, poetry, etc.)
-poetry install
+curl -LsSf https://astral.sh/uv/install.sh | sh
+source $HOME/.local/bin/env
+export PYTHONPATH=/project/code
+cd /project/code
+uv run pytest
 
-# Convert static asset files
-poetry run python3 manage.py collectstatic --no-input
+make install && make collectstatic && make compilemessages && make migrate
 
-# Apply any outstanding database migrations
-poetry run python3 manage.py migrate
-
-
-
-
-
-##!/usr/bin/env bash
-#
-#curl -LsSf https://astral.sh/uv/install.sh | sh
-#source $HOME/.local/bin/env
-#export PYTHONPATH=/project/code
-#cd /project/code
-#uv run pytest
-#
-#make install && make collectstatic && make compilemessages && make migrate
-#
