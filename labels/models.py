b5a625dd95fd02f65ed0324be85634f96d6e3c8a
robert <Neorganizm911@yandex.ru>
2025-06-24 00:23:04 +0300

    8 step

--- labels/models.py
+++ labels/models.py
@@ -1,8 +1,9 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
 class Label(models.Model):
-    name = models.CharField(max_length=255, unique=True)
+    name = models.CharField(_('Имя'), max_length=255, unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
