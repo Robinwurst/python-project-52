8612cb3540a07738bca6366e7d5682e94e69b8ea
robert <Neorganizm911@yandex.ru>
2025-06-23 23:35:27 +0300

    8 step

--- users/forms.py
+++ users/forms.py
@@ -10,6 +10,38 @@ class UserCreateForm(UserCreationForm):
 
 
 class UserUpdateForm(forms.ModelForm):
+    password1 = forms.CharField(
+        label='Пароль',
+        widget=forms.PasswordInput,
+        required=False
+    )
+    password2 = forms.CharField(
+        label='Подтверждение пароля',
+        widget=forms.PasswordInput,
+        required=False
+    )
+
     class Meta:
         model = User
-        fields = ['username', 'first_name', 'last_name', 'email']
+        fields = ['username', 'first_name', 'last_name']
+
+    def clean(self):
+        cleaned_data = super().clean()
+        password1 = cleaned_data.get("password1")
+        password2 = cleaned_data.get("password2")
+
+        if password1 or password2:
+            if password1 != password2:
+                raise forms.ValidationError("Пароли не совпадают")
+        return cleaned_data
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        password1 = self.cleaned_data["password1"]
+
+        if password1:
+            user.set_password(password1)
+
+        if commit:
+            user.save()
+        return user
