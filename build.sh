71d4c4388d07719e9e14c83a218e0176efd76e49
robert <Neorganizm911@yandex.ru>
2025-06-23 20:09:16 +0300

    8 step

--- build.sh
+++ build.sh
@@ -1,19 +1,9 @@
 #!/usr/bin/env bash
-# скачиваем uv
+
 curl -LsSf https://astral.sh/uv/install.sh | sh
 source $HOME/.local/bin/env
+export PYTHONPATH=/project/code
+cd /project/code
+uv run pytest
 
-# здесь добавьте все необходимые команды для установки вашего проекта
-# команду установки зависимостей, сборки статики, применения миграций и другие
-make install && make collectstatic && make migrate
-
-
-##!/usr/bin/env bash
-#
-#curl -LsSf https://astral.sh/uv/install.sh | sh
-#source $HOME/.local/bin/env
-#export PYTHONPATH=/project/code
-#cd /project/code
-#uv run pytest
-#
-#make install && make collectstatic && make compilemessages && make migrate
+make install && make collectstatic && make compilemessages && make migrate
