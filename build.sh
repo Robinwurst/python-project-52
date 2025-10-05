9149c8141d3c6ffc92b0316c2036ab0261668343
robert <Neorganizm911@yandex.ru>
2025-10-05 19:50:33 +0300

    8 step

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-curl -LsSf https://astral.sh/uv/install.sh | sh
-source $HOME/.local/bin/env
-export PYTHONPATH=/project/code
-cd /project/code
-
-
-make install && make collectstatic && make compilemessages && make migrate
